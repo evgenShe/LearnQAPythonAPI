d8e11ff3ec2b7b1dc90dfb9ec4a2e2c2260546b5
evgeniysheremet <e.sheremet@ibolit.pro>
2023-11-23 22:22:17 +0300

    add Ex11

--- Ex11.py
+++ Ex11.py
@@ -4,6 +4,7 @@ import requests
 class TestHomeWorkCookie:
     def test_homework_cookie(self):
         response = requests.get("https://playground.learnqa.ru/api/homework_cookie")
-        print(response.cookies)
+        # print(response.cookies)
         cookie = response.cookies.get("HomeWork")
+        # print(cookie)
         assert cookie == "hw_value", "There is no 'hw_value' in the response"
