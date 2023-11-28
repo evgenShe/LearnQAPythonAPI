1fd1585b522fad55728200c424d2cf4842fcb45c
evgeniysheremet <e.sheremet@ibolit.pro>
2023-11-28 17:20:45 +0300

    fix

--- homework/Ex13.py
+++ homework/Ex13.py
@@ -1,129 +1,52 @@
-import requests
-
-from lib.assertions import Assertions
 import pytest
+import requests
+import json
+from lib.assertions import Assertions
 
 
 class TestUserAgent:
     exclude_params = [
         ('Mozilla/5.0 (Linux; U; Android 4.0.2; en-us; Galaxy Nexus Build/ICL53F) '
-         'AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30'),
+         "AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30",
+         'Mobile', 'No', 'Android'),
         ('Mozilla/5.0 (iPad; CPU OS 13_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) '
-         'CriOS/91.0.4472.77 Mobile/15E148 Safari/604.1'),
-        ('Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)'),
+         'CriOS/91.0.4472.77 Mobile/15E148 Safari/604.1',
+         'Mobile', 'Chrome', 'iOS'),
+        ('Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)',
+         'Googlebot', 'Unknown', 'Unknown'),
         ('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
-         'Chrome/91.0.4472.77 Safari/537.36 Edg/91.0.100.0'),
-        ('Mozilla/5.0 (iPad; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) '
-         'Version/13.0.3 Mobile/15E148 Safari/604.1')
+         'Chrome/91.0.4472.77 Safari/537.36 Edg/91.0.100.0',
+         'Web', 'Chrome', 'No'),
+        ('Mozilla/5.0 (iPad; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 '
+         '(KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1',
+         'Mobile', 'No', 'iPhone')
     ]
 
-    @pytest.mark.parametrize("condition", exclude_params)
-    def test_user_agent(self, condition):
-        response = requests.get(
-            "https://playground.learnqa.ru/ajax/api/user_agent_check",
-            headers={"User-Agent": condition}
-        )
-        platform = response.json()["platform"]
-        browser = response.json()["browser"]
-        device = response.json()["device"]
-        print(platform)
+    @pytest.mark.parametrize('user_agent', exclude_params)
+    def test_user_agent(self, user_agent):
+        response = requests.get("https://playground.learnqa.ru/ajax/api/user_agent_check",
+                                headers={"User-Agent": user_agent[0]})
 
-        if condition == ('Mozilla/5.0 (Linux; U; Android 4.0.2; en-us; Galaxy Nexus Build/ICL53F) '
-                         'AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30'):
-            Assertions.assert_json_value_by_name(
-                response,
-                "platform",
-                'Mobile',
-                f"User agent fail platform {platform}"
-            )
-            Assertions.assert_json_value_by_name(
-                response,
-                'browser',
-                'No',
-                f"User agent fail browser {browser}"
-            )
-            Assertions.assert_json_value_by_name(
-                response,
-                'device',
-                'Android',
-                f"User agent fail device {device}"
-            )
-        elif condition == ('Mozilla/5.0 (iPad; CPU OS 13_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) '
-                           'CriOS/91.0.4472.77 Mobile/15E148 Safari/604.1'):
-            Assertions.assert_json_value_by_name(
-                response,
-                "platform",
-                'Mobile',
-                f"User agent fail platform {platform}"
-            )
-            Assertions.assert_json_value_by_name(
-                response,
-                'browser',
-                'Chrome',
-                f"User agent fail browser {browser}"
-            )
-            Assertions.assert_json_value_by_name(
-                response,
-                'device',
-                'iOS',
-                f"User agent fail device {device}"
-            )
-        elif condition == ('Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)'):
-            Assertions.assert_json_value_by_name(
-                response,
-                "platform",
-                'Googlebot',
-                f"User agent fail platform {platform}"
-            )
-            Assertions.assert_json_value_by_name(
-                response,
-                'browser',
-                'Unknown',
-                f"User agent fail browser {browser}"
-            )
-            Assertions.assert_json_value_by_name(
-                response,
-                'device',
-                'Unknown',
-                f"User agent fail device {device}"
-            )
-        elif condition == ('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
-                           'Chrome/91.0.4472.77 Safari/537.36 Edg/91.0.100.0'):
-            Assertions.assert_json_value_by_name(
-                response,
-                "platform",
-                'Web',
-                f"User agent fail platform {platform}"
-            )
-            Assertions.assert_json_value_by_name(
-                response,
-                'browser',
-                'Chrome',
-                f"User agent fail browser {browser}"
-            )
-            Assertions.assert_json_value_by_name(
-                response,
-                'device',
-                'No',
-                f"User agent fail device {device}"
-            )
-        elif condition == ('Mozilla/5.0 (iPad; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 '
-                           '(KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1'):
-            Assertions.assert_json_value_by_name(
-                response,
-                "platform",
-                'Mobile',
-                f"User agent fail platform {platform}"
-            )
-            Assertions.assert_json_value_by_name(
-                response,
-                'browser',
-                'No',
-                f"User agent fail browser {browser}"
-            )
-            Assertions.assert_json_value_by_name(
-                response,
-                'device',
-                'iPhone',
-                f"User agent fail device {device}"
-            )
+        platform = response.json()['platform']
+        browser = response.json()['browser']
+        device = response.json()['device']
+
+        Assertions.assert_json_value_by_name(
+            response,
+            "platform",
+            user_agent[1],
+            f"User agent '{user_agent[1]}' does not match the parameter platform: '{platform}'"
+
+        )
+        Assertions.assert_json_value_by_name(
+            response,
+            'browser',
+            user_agent[2],
+            f"User agent '{user_agent[2]}' does not match the parameter browser: '{browser}'"
+        )
+        Assertions.assert_json_value_by_name(
+            response,
+            'device',
+            user_agent[3],
+            f"User agent '{user_agent[3]}' does not match the parameter device: '{device}'"
+        )
