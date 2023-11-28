a793cb43adef07907f58e49b9f294e2d7e670767
evgeniysheremet <e.sheremet@ibolit.pro>
2023-11-28 17:35:04 +0300

    fix2

--- homework/Ex12.py
+++ homework/Ex12.py
@@ -6,6 +6,9 @@ from lib.base_case import BaseCase
 class TestHomeWorkHeader(BaseCase):
     def test_homework_header(self):
         response = requests.get("https://playground.learnqa.ru/api/homework_header")
+
+        assert response.headers.get("x-secret-homework-header")
+
         cookie_value = self.get_header(response, "x-secret-homework-header")
 
         assert cookie_value == "Some secret value", "There is no 'Some secret value' in the response"
